125588223b2a6e864276d945b84558cda77f6d82
kubix1605 <kubix1605@gmail.com>
2023-10-14 15:18:50 +0200

    index.php Update

--- index.php
+++ index.php
@@ -1,3 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="HTML, CSS">
+    <meta name="description" content="Information about my website">
+    <title>Document</title>
+
+    <title>My web page</title>
+        <style>
+            img {
+                width: 100px;
+                border-radius: 50px;
+                float: left;
+                margin-right: 10px;
+                object-fit: cover;
+            }
+            .username {
+                font-weight: bold;
+            }
+        </style>
+</head>
+<body>
+    <img src="https://sklep-ballantines.pl/3979-large_default/rum-la-progresiva-de-vigia.jpg" alt="alco image">
+    <p class ="username">@Jojo</p>
+    <p>I love drinking </p>
+</body>
+</html>
+
 <?php
 
 echo 'Hi there 👋';
